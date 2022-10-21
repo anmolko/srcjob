4ee2143882423b4e596040d16771466e39c174dd
surajmah <suraj.canosoft@gmail.com>
2022-10-21 14:50:32 +0545

    update 404 error design

--- resources/views/error/404.blade.php
+++ resources/views/error/404.blade.php
@@ -2,55 +2,63 @@
 @section('title')  Page Not Found @endsection
 @section('content')
   
-    <!-- Error Page Area start -->
-    <section class="error-page-area py-130 rel z-1">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-5">
-                    <div class="error-content rmb-75 wow fadeInLeft delay-0-2s">
-                        
-                        <h1>OPPS!</h1>
-                        <h3>This Page Can’t be Found</h3>
-                        <p>Start building your first prototype in no time. Ogency intuitive drag & drop interface gives you all the building blocks</p>
-                        <a href="/" class="theme-btn style-two mt-15 mb-50">Go to Home <i class="fas fa-angle-double-right"></i></a>
-                        <div class="follow-us">
-                            <h4>Follow Us</h4>
-                            <div class="social-style-two">
-                                @if(!empty(@$setting_data->facebook))
-                                  <a href="@if(!empty(@$setting_data->facebook)) {{@$setting_data->facebook}} @endif" target="_blank" class="social-fb"
-                                  ><i class="fab fa-facebook-f"></i
-                                  ></a>
-                                @endif
-                                @if(!empty(@$setting_data->youtube))
 
-                                  <a href="@if(!empty(@$setting_data->youtube)) {{@$setting_data->youtube}} @endif" target="_blank" class="social-youtube"
-                                  ><i class="fab fa-youtube"></i
-                                  ></a>
-                                @endif
-                                @if(!empty(@$setting_data->instagram))
+        <!--Page Header Start-->
+        <section class="page-header">
+            <div class="page-header__bg" style="background-image: url({{asset('assets/frontend/images/backgrounds/page-header-bg.jpg')}});">
+            </div>
+            <div class="page-header__shape-one float-bob-x-2"></div>
+            <div class="page-header__shape-2 float-bob-y">
+                <img src="{{asset('assets/frontend/images/shapes/page-header-shape-2.png')}}" alt="">
+            </div>
+            <div class="page-header__shape-3 float-bob-x">
+                <img src="{{asset('assets/frontend/images/shapes/page-header-shape-3.png')}}" alt="">
+            </div>
+            <div class="page-header__shape-4 float-bob-y">
+                <img src="{{asset('assets/frontend/images/shapes/page-header-shape-4.png')}}" alt="">
+            </div>
+            <div class="container">
+                <div class="page-header__inner text-left">
+                    <ul class="thm-breadcrumb list-unstyled">
+                        <li><a href="/">Home</a></li>
+                        <li>404 Error</li>
+                    </ul>
+                    <h2>404 Error</h2>
+                </div>
+            </div>
+        </section>
+        <!--Page Header End-->
+        
 
-                                  <a href="@if(!empty(@$setting_data->instagram)) {{@$setting_data->instagram}} @endif" target="_blank" class="social-instagram"
-                                  ><i class="fab fa-instagram"></i
-                                  ></a>
-                                @endif
-                                @if(!empty(@$setting_data->linkedin))
-
-                                  <a href="@if(!empty(@$setting_data->linkedin)) {{@$setting_data->linkedin}} @endif" target="_blank" class="social-linkedin"
-                                  ><i class="fab fa-linkedin-in"></i
-                                  ></a>
-                                @endif
+        <!--Error Page Start-->
+        <section class="error-page">
+            <div class="error-page-shape-bg float-bob-x"
+                style="background-image: url({{asset('assets/frontend/images/shapes/error-page-shape-bg.png')}});"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="error-page__inner">
+                            <div class="error-page__title-box">
+                                <h2 class="error-page__title">404</h2>
+                            </div>
+                            <h3 class="error-page__tagline">Sorry! Page Not Found</h3>
+                            <p class="error-page__text">The page you are looking for was never existed.</p>
+                          
+                            <a href="/" class="thm-btn error-page__btn">Back To Home </a>
+                            <div class="error-page-shape-1 float-bob-y">
+                                <img src="{{asset('assets/frontend/images/shapes/error-page-shape-1.png')}}" alt="">
+                            </div>
+                            <div class="error-page-shape-2 float-bob-x">
+                                <img src="{{asset('assets/frontend/images/shapes/error-page-shape-2.png')}}" alt="">
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-7">
-                    <div class="error-image wow fadeInRight delay-0-2s">
-                        <img src="{{asset('assets/frontend/images/background/404.png')}}" alt="404 Error">
-                    </div>
-                </div>
             </div>
-        </div>
-    </section>
-    <!-- Error Page Area end -->
+        </section>
+        <!--Error Page End-->
+
+
+
 
 @endsection
