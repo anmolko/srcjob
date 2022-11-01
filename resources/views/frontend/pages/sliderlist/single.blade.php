fdf6d04fc12108f9144878ca3e227b2920d6c623
surajmah <suraj.canosoft@gmail.com>
2022-11-01 15:14:04 +0545

    updated slider list

--- resources/views/frontend/pages/sliderlist/single.blade.php
+++ resources/views/frontend/pages/sliderlist/single.blade.php
@@ -25,51 +25,65 @@
 
 @section('content')
 
-	        <!-- Page Banner Start -->
-			<section class="page-banner-area pt-245 rpt-150 pb-170 rpb-100 rel z-1 bgc-lighter text-center">
+
+        <!--Page Header Start-->
+        <section class="page-header">
+            <div class="page-header__bg" style="background-image: url({{asset('assets/frontend/images/backgrounds/page-header-bg.jpg')}});">
+            </div>
+            <div class="page-header__shape-one float-bob-x-2"></div>
+            <div class="page-header__shape-2 float-bob-y">
+                <img src="{{asset('assets/frontend/images/shapes/page-header-shape-2.png')}}" alt="">
+            </div>
+            <div class="page-header__shape-3 float-bob-x">
+                <img src="{{asset('assets/frontend/images/shapes/page-header-shape-3.png')}}" alt="">
+            </div>
+            <div class="page-header__shape-4 float-bob-y">
+                <img src="{{asset('assets/frontend/images/shapes/page-header-shape-4.png')}}" alt="">
+            </div>
             <div class="container">
-                <div class="banner-inner rpt-10">
-                    <!-- <h1 class="page-title wow fadeInUp delay-0-2s">{{ @$singleBlog->title }}</h1> -->
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb justify-content-center wow fadeInUp delay-0-4s">
-                            <li class="breadcrumb-item"><a href="/">Home</a></li>
-                            <li class="breadcrumb-item"><a href="{{url('/'.@$singleSlider->section->page->slug)}}">{{ucwords(@$singleSlider->section->page->name)}}</a></li>
-                            <li class="breadcrumb-item active">{{ucwords(@$singleSlider->list_header)}}</li>
-                        </ol>
-                    </nav>
+                <div class="page-header__inner text-left">
+                    <ul class="thm-breadcrumb list-unstyled">
+                        <li><a href="/">Home</a></li>
+                        <li> <a href="{{url('/'.@$singleSlider->section->page->slug)}}">{{ucwords(@$singleSlider->section->page->name)}}</a></li>
+                        <li> <a href="#">{{ucwords(@$singleSlider->list_header)}}</a></li>
+                    </ul>
+                    <h2>{{ucwords(@$singleSlider->list_header)}}</h2>
                 </div>
             </div>
-            <div class="banner-shapes">
-                <div class="circle wow zoomInLeft delay-0-2s" data-wow-duration="2s"></div>
-                <img class="shape-one" src="{{asset('assets/frontend/images/shapes/hero-shape1.png')}}" alt="Shape">
-                <img class="shape-two" src="{{asset('assets/frontend/images/shapes/hero-shape2.png')}}" alt="Shape">
-            </div>
         </section>
-        <!-- Page Banner End -->
+        <!--Page Header End-->
 
-		        <!-- Blog Details Area start -->
-				<section class="blog-details-area py-130 rel z-1">
+
+        <!-- Blog Details Area start -->
+           <!--Blog Details Start-->
+           <section class="blog-details">
             <div class="container">
-                <div class="row medium-gap">
-                    <div class="col-lg-8">
-                        <div class="blog-details-content wow fadeInUp delay-0-2s">
-                            <div class="image mb-45">
-                                <img src="{{ asset('/images/section_elements/list_1/'.$singleSlider->list_image) }}" alt="{{@$singleSlider->list_header}}">
+                <div class="row">
+                    <div class="col-xl-8 col-lg-7">
+                        <div class="blog-details__left">
+                            <div class="blog-details__inner">
+                                <div class="blog-details__single">
+                                <div class="blog-list__img-box">
+                                        <div class="blog-list__img">
+                                            <img src="{{ asset('/images/section_elements/list_1/'.$singleSlider->list_image) }}" alt="{{@$singleSlider->list_header}}">
+                                        </div>
+                                    </div>
+                                    <div class="blog-list__content">
+                                        <h4 class="blog-list__title">{{ ucwords(@$singleSlider->list_header) }}</h4>
+                                        {{@$singleSlider->list_description}}
+                                    </div>
+                                </div>
                             </div>
-                            <h3>{{ucwords(@$singleSlider->list_header)}}</h3>
-                          
-                            {{@$singleSlider->list_description}}
-
                         </div>
-                       
                     </div>
-                    <div class="col-lg-4 col-md-7 col-sm-9">
-					   @include('frontend.pages.sliderlist.sidebar')            
-                  
+                    <div class="col-xl-4 col-lg-5">
+                        @include('frontend.pages.sliderlist.sidebar')
                     </div>
                 </div>
             </div>
         </section>
-        <!-- Blog Details Area end -->
+        <!--Blog Details End-->
+
+
 		
 @endsection
